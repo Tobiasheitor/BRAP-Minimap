d5cbce81d2df60a0247a49803a3b5eaa7f71f465
Tobias Heitor Goettert <tobias.goettert@compasso.com.br>
2021-07-01 15:46:58 -0300

    Ajuste

--- minimap.user.js
+++ minimap.user.js
@@ -3,19 +3,19 @@
 // @namespace    BRAP Minimap
 // @version      2.0.0
 // @description  Minimap Oficial BRAP
-// @author       test
+// @author       yOriowm#2260
 // @match        https://pixelcanvas.io/*
 // @match        http://pixelcanvas.io/*
-// @homepage     https://github.com/Tobiasheitor/BRAP-Minimap
-// @updateURL    https://raw.githubusercontent.com/Tobiasheitor/BRAP-Minimap/master/minimap.user.js
-// @downloadURL  https://raw.githubusercontent.com/Tobiasheitor/BRAP-Minimap/master/minimap.user.js
+// @homepage     https://github.com/yOriowm/BRAP-Minimap
+// @updateURL    https://raw.githubusercontent.com/yOriowm/BRAP-Minimap/master/minimap.user.js
+// @downloadURL  https://raw.githubusercontent.com/yOriowm/BRAP-Minimap/master/minimap.user.js
 // @grant        none
 // ==/UserScript==
 
 //ref:https://stackoverflow.com/questions/4604663/download-single-files-from-github
 //github raw file url  template
 //https://raw.githubusercontent.com/user/repository/branch/filename
-//https://raw.githubusercontent.com/Tobiasheitor/BRAP-Minimap/master/README.md
+//https://raw.githubusercontent.com/yOriowm/BRAP-Minimap/master/README.md
 
 window.baseTepmlateUrl =
   "https://raw.githubusercontent.com/Tobiasheitor/BRAP-Minimap/master";
@@ -201,7 +201,7 @@ function exportMd() {
     var eles = template_list[index];
     mdstr += "\n#### " + index;
     mdstr +=
-      "\n[![](https://raw.githubusercontent.com/Tobiasheitor/BRAP-Minimap/master/images/" +
+      "\n[![](https://raw.githubusercontent.com/yOriowm/BRAP-Minimap/master/images/" +
       eles.name +
       ")]";
     mdstr +=
